b23cf49346279b7a0be2fcad5cd323cce8c107e7
Christopher Getschmann <christopher@volzo.de>
2020-09-16 12:00:09 +0200

    removed timebox I2C code

--- software/ccam/buildroot_install.sh
+++ software/ccam/buildroot_install.sh
@@ -1,8 +1,5 @@
 pip3 install -r /home/pi/ccam/requirements.txt
 
-# i2c required for the Adafruit Bonnet OLED display
-grep -qxF 'dtparam=i2c_arm=on' /boot/config.txt || echo 'dtparam=i2c_arm=on' >> /boot/config.txt
-
 # start_x=1 to load the extended GPU firmware required by the PiCamera for trashcam
 # but apparently that's not required (actually pi wont boot is start_x=1 is set)
 # because buildroot just sneakily replaces and renames the firmware files instead of 
