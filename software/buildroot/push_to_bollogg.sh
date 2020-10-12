a0680ee6f331f4ad66332d4734f393679355dd00
Christopher Getschmann <christopher@volzo.de>
2020-10-12 20:51:28 +0200

    buildroot: custom post-build and post-image scripts

--- software/buildroot/push_to_bollogg.sh
+++ software/buildroot/push_to_bollogg.sh
@@ -2,4 +2,11 @@
 
 DIR="bollogg:/home/volzotan/buildroot_extree/external_tree/"
 
-rsync -av config_raspberrypi0w/ bollogg:/home/volzotan/buildroot_extree/external_tree/ --delete --exclude=".DS_Store"
+rsync -av config_raspberrypi0w/ $DIR --delete --exclude=".DS_Store"
+
+rsync -av ../ccam $DIR"overlay/home/pi"             \
+    --delete                                        \
+    --exclude=".DS_Store"                           \
+    --exclude="README.md"                           \
+    --exclude="*.pyc"                               \
+    --exclude="*__pycache__*"                           
